01ed09e7dfc93bf0e1242232f397ed97b3c1db97
Yash25032003 <yashsharma210300@acropolis.in>
2024-12-15 19:19:49 +0530

    add login controller

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -62,10 +62,44 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     );
 
     // response
-    res.json({ accessToken: token });
+    res.status(201).json({ accessToken: token });
   } catch (err) {
     return next(createHttpError(500, "error while signing the jwt token"));
   }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  // validation of fields
+  if (!email || !password) {
+    return next(createHttpError("400", "All fields are required"));
+  }
+
+  // find user in DB
+
+  try {
+    const user = await userModel.findOne({ email: email });
+    if (!user) {
+      return next(createHttpError(404, "User not found."));
+    }
+
+    // verify the password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return next(createHttpError(400, "Username or password incorrect!"));
+    }
+
+    // access token generate
+    const token = sign({ sub: (await user)._id }, config.jwtsecret as string, {
+      expiresIn: "7d",
+    });
+    // response
+    res.status(201).json({ accessToken: token });
+  } catch (err) {
+    return next(createHttpError("401", "Error found"));
+  }
+};
+
+export { createUser, loginUser };
