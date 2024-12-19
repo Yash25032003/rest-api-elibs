f74d4ae33a6eb7674431fe775b6a6d03c059fd64
Yash25032003 <yashsharma210300@acropolis.in>
2024-12-19 19:21:17 +0530

    add setup multer

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,7 +1,22 @@
 import express from "express";
+import multer from "multer";
+import path from "node:path"
 import { createBook } from "./bookController";
 const bookRouter = express.Router();
 
-bookRouter.post("/", createBook);
+//! Multer ke through hum form data ko handle kar sakte hai 
+
+// upload jo hai vo file upload handle kar raha hai
+const upload = multer({
+    dest: path.resolve(__dirname , '../../public/data/uploads'),
+    limits:{fileSize: 3e7} // 30MB
+})
+
+// .fields se hum multiple files upload kar sakte hai
+bookRouter.post("/", upload.fields([
+    {name: "coverImage" , maxCount: 1},
+    {name: "file" , maxCount: 1}
+
+]) ,  createBook);
 
 export default bookRouter;
