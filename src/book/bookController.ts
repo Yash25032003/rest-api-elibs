515ddc05b82ef99f3a0263192e4f0355a9adf4a4
Yash25032003 <yashsharma210300@acropolis.in>
2025-01-08 13:00:59 +0530

    add create newBook

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,10 +2,13 @@ import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
+import fs from "node:fs";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   console.log("files", req.files);
   // res.json({ message: "Files Submiitted" });
+  const { title, genre } = req.body;
 
   try {
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
@@ -17,13 +20,14 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
       fileName
     );
 
+    // uploading and handling images in cloudinary
     const uploadResult = await cloudinary.uploader.upload(filePath, {
       filename_override: fileName,
       folder: "book-covers",
       format: coverImageMimeType,
     });
 
-    // handling pdf
+    // handling pdf in cloudinary
     const bookFileName = files.file[0].filename;
     const bookFilePath = path.resolve(
       __dirname,
@@ -41,10 +45,29 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
       }
     );
 
+    // Database me send kar rahe hai newBook ko jo user ne cloudinary pe upload ki hai 
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "675ebacf999105d831382f23",
+      coverImage: uploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+
+    // delete temperory files after uploading of files is completed
+    try {
+      await fs.promises.unlink(filePath);
+      await fs.promises.unlink(bookFilePath);
+    } catch (error) {
+      console.log(error);
+    }
+
+    res.status(201).json({ id: newBook._id });
+
     console.log("Uploaded file", uploadResult);
     console.log("Uploaded file", bookFileUploadResult);
 
-    res.json({ message: "Files Submiitted" });
+    // res.json({ message: "Files Submiitted" });
   } catch (error) {
     console.log(error);
     return next(
